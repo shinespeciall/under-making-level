22e6d4cf1e2dcf93e037ae6cfeb8202c620655d1
Chen Yifeng <648616375@qq.com>
2021-02-05 12:20:28 +0800

    Update WarioSpeedResetWithCondCheck.c

--- WarioSpeedResetWithCondCheck.c
+++ WarioSpeedResetWithCondCheck.c
@@ -1,25 +1,39 @@
-// @Description Allows Wario Speed Reset With Condition Checks
+// @Description Wario can now throw enemies without losing his speed
 // @HookAddress 0x12BB2
 // Mode: Thumb
 // Made by ssp(shinespeciall)
-// Verison 1.0
-// supported compiler: GCC: (GNU Arm Embedded Toolchain 9-2020-q2-update) 9.3.1 20200408 (release)
+// Verison 1.1
+
+/** Changelog:
+ * v1.1
+ * rewrite the hookstring and the way the function recepting variable
+ * the patch is now compatible with every version of arm7 gcc compiler
+ *  
+ * v1.0
+ * Implement the feture and wario can throw enemies without losing his speed
+ */
 
 /* Hook details
 ------------------------- this only works when the new function push lr at first and bx lr at last.
-0x8012BB2: LDR R0, 0x8012BBF    | 4803
-0x8012BB4: MOV LR, R0           | 4686
-0x8012BB6: LDR R0, pointer      | 4801
-0x8012BB8: BX R0                | 4700
-0x8012BBA: B 0x8012BC6          | E002
-0x8012BCC: .fill a pointer      | 0XXXXXXX ; end with 1 (in binary) to make it run in thumb mode
-0x8012BC0: .fill BB2B0108       | 08012BBB ; end with 1 (in binary) to make it run in thumb mode
-0x8012BC2: nop                  | 46C0‬     ; i.e. mov r8, r8
-0x8012BC4: nop....              | 46C0
+0x8012BB2: PUSH R4              | B410
+0x8012BB4: MOV R0, R4           | 1C20
+0x8012BB6: LDR R1, 0x8012BBB    | 4903
+0x8012BB8: MOV LR, R1           | 468E
+0x8012BBA: LDR R1, pointer      | 4901
+0x8012BBC: BX R1                | 4708
+0x8012BBE: B 0x8012BC8          | E002
+0x8012BC0: .fill a pointer      | 0XXXXXXX ; end with 1 (in binary) to make it run in thumb mode
+0x8012BC4: .fill C92B0108       | 08012BC9 ; end with 1 (in binary) to make it run in thumb mode
+0x8012BC8: POP R4               | BC10
+0x8012BCA: NOP                  | 46C0
+0x8012BCC: NOP                  | 46C0
+0x8012BCE: NOP                  | 46C0
+0x8012BD0: NOP                  | 46C0
+0x8012BD2: NOP                  | 46C0
 -------------------------
 Hook String:
-@HookString 03 48 86 46 01 48 00 47 02 E0 P BB 2B 01 08 C0 46 C0 46 C0 46 C0 46 C0 46 C0 46 C0 46 C0 46
-count:                                     ^14                                                         ^34
+@HookString 10 B4 20 1C 03 49 8E 46 01 49 08 47 02 E0 P C9 2B 01 08 10 BC C0 46 C0 46 C0 46 C0 46 C0 46
+count:                                                                                                 ^34
 Hook length: 34 Bytes
 */
 
@@ -35,15 +49,14 @@ Hook length: 34 Bytes
 #define Sub_800FD28_WarioSpeedReset ((void (*)()) 0x800FD29)
 
 __attribute__((no_caller_saved_registers))
-void WarioSpeedResetWithCondCheck()
+void WarioSpeedResetWithCondCheck(unsigned char data)
 {
-    /**WarioState shoulb be normal state 
+    /**WarioState should be in normal state 
      * WarioAnimation is always 4 before Wario throwing the item, it always reset speed first then set WarioAnimation to 38 (midair throw)
      * u8_WarioHoldItem == 4 escape the cases which Wario is not holding something
-     * R4 Get the next frame WarioAnimation flag, this trigger the skip finally
+     * R4 Get the next frame WarioAnimation flag in the vanilla code.
+     * we fetch it using the patch function's parameter passing, which triggers the skip finally
      */
-    unsigned char data = 0;
-    asm volatile ("mov %0, r4\n\t" : "=r" (data));
 
     if (!((WarioState == 0 && WarioAnimation == 4 && u8_WarioHoldItem == 4 && data == 0x26))) {
         Sub_800FD28_WarioSpeedReset();
